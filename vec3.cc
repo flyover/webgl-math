e44db7f280479395da99fa6cabe199bc7aa0651b
Isaac Burns <isaacburns@gmail.com>
2015-09-17 16:46:05 -0500

    refactor for node 4.x

--- vec3.cc
+++ vec3.cc
@@ -2,21 +2,27 @@
 #include <node.h>
 #include <v8.h>
 
-#if defined(IOJS_3_0_MODULE_VERSION) && (NODE_MODULE_VERSION >= IOJS_3_0_MODULE_VERSION)
-
-#define IS_VEC2(value) (value->IsFloat32Array() && (value.As<v8::Float32Array>()->Length() == 3))
-
-#define IS_VEC2_ARRAY(value) (value->IsFloat32Array() && (value.As<v8::Float32Array>()->Length() % 3 == 0))
-
-#else
+#if NODE_MODULE_VERSION <= NODE_0_12_MODULE_VERSION
 
 #define IS_VEC3(value) (value->IsObject() \
 	&& (value.As<v8::Object>()->GetIndexedPropertiesExternalArrayDataType() == v8::kExternalFloatArray) \
-	&& (value.As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength() == (3 * sizeof(float))))
+	&& (value.As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength() == 3))
+#define GET_VEC3_DATA(value) ((float*) value.As<v8::Object>()->GetIndexedPropertiesExternalArrayData())
 
 #define IS_VEC3_ARRAY(value) (value->IsObject() \
 	&& (value.As<v8::Object>()->GetIndexedPropertiesExternalArrayDataType() == v8::kExternalFloatArray) \
-	&& (value.As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength() % (3 * sizeof(float)) == 0))
+	&& ((value.As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength() % 3) == 0))
+#define GET_VEC3_ARRAY_DATA(value) GET_VEC3_DATA(value)
+#define GET_VEC3_ARRAY_LENGTH(value) (value.As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength())
+
+#else
+
+#define IS_VEC3(value) (value->IsFloat32Array() && (value.As<v8::Float32Array>()->Length() == 3))
+#define GET_VEC3_DATA(value) (float*)(static_cast<char*>(value.As<v8::Float32Array>()->Buffer()->GetContents().Data()) + value.As<v8::Float32Array>()->ByteOffset())
+
+#define IS_VEC3_ARRAY(value) (value->IsFloat32Array() && ((value.As<v8::Float32Array>()->Length() % 3) == 0))
+#define GET_VEC3_ARRAY_DATA(value) GET_VEC3_DATA(value)
+#define GET_VEC3_ARRAY_LENGTH(value) (value.As<v8::Float32Array>()->Length())
 
 #endif
 
@@ -28,17 +34,10 @@ NAN_METHOD(muls_add)
 	if (!IS_VEC3(info[1])) return Nan::ThrowError("a not a vec3");
 	if (!IS_VEC3(info[2])) return Nan::ThrowError("b not a vec3");
 	if (!(info[3]->IsNumber())) return Nan::ThrowError("s not a number");
-	#if defined(IOJS_3_0_MODULE_VERSION) && (NODE_MODULE_VERSION >= IOJS_3_0_MODULE_VERSION)
-	float* out = (float*) info[0].As<v8::Float32Array>()->Buffer()->GetContents().Data();
-	const float* a = (const float*) info[1].As<v8::Float32Array>()->Buffer()->GetContents().Data();
-	const float* b = (const float*) info[2].As<v8::Float32Array>()->Buffer()->GetContents().Data();
-	const float s = (const float) info[3]->NumberValue();
-	#else
-	float* out = (float*) info[0].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
-	const float* a = (float*) info[1].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
-	const float* b = (float*) info[2].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
-	const float s = (const float) info[3]->NumberValue();
-	#endif
+	float* out = GET_VEC3_DATA(info[0]);
+	const float* a = GET_VEC3_DATA(info[1]);
+	const float* b = GET_VEC3_DATA(info[2]);
+	const float s = (float) info[3]->NumberValue();
 	out[0] = a[0] + (s * b[0]);
 	out[1] = a[1] + (s * b[1]);
 	out[2] = a[2] + (s * b[2]);
@@ -53,20 +52,12 @@ NAN_METHOD(muls_add)
 	if (!IS_VEC3_ARRAY(info[1])) return Nan::ThrowError("a not a vec3.array");
 	if (!IS_VEC3_ARRAY(info[2])) return Nan::ThrowError("b not a vec3.array");
 	if (!(info[3]->IsNumber())) return Nan::ThrowError("s not a number");
-	#if defined(IOJS_3_0_MODULE_VERSION) && (NODE_MODULE_VERSION >= IOJS_3_0_MODULE_VERSION)
-	float* out = (float*) info[0].As<v8::Float32Array>()->Buffer()->GetContents().Data();
-	const float* a = (const float*) info[1].As<v8::Float32Array>()->Buffer()->GetContents().Data();
-	const float* b = (const float*) info[2].As<v8::Float32Array>()->Buffer()->GetContents().Data();
-	const float s = (const float) info[3]->NumberValue();
-	const size_t n = info[0].As<v8::Float32Array>()->Length();
-	#else
-	float* out = (float*) info[0].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
-	const float* a = (float*) info[1].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
-	const float* b = (float*) info[2].As<v8::Object>()->GetIndexedPropertiesExternalArrayData();
-	const float s = (const float) info[3]->NumberValue();
-	const int n = info[0].As<v8::Object>()->GetIndexedPropertiesExternalArrayDataLength() / sizeof(float);
-	#endif
-	for (int i = 0; i < n; ++i)
+	float* out = GET_VEC3_ARRAY_DATA(info[0]);
+	const float* a = GET_VEC3_ARRAY_DATA(info[1]);
+	const float* b = GET_VEC3_ARRAY_DATA(info[2]);
+	const float s = (float) info[3]->NumberValue();
+	const size_t n = GET_VEC3_ARRAY_LENGTH(info[0]);
+	for (size_t i = 0; i < n; ++i)
 	{
 		out[i] = a[i] + (s * b[i]);
 	}
@@ -77,7 +68,6 @@ NAN_METHOD(muls_add)
 
 NAN_MODULE_INIT(init)
 {
-	Nan::HandleScope scope;
 	Nan::SetMethod(target, "muls_add", muls_add);
 	v8::Local<v8::Object> array = Nan::New<v8::Object>();
 	Nan::Set(target, Nan::New<v8::String>("array").ToLocalChecked(), array);
@@ -86,8 +76,4 @@ NAN_MODULE_INIT(init)
 
 } // namespace node_vec3
 
-#if NODE_VERSION_AT_LEAST(0,11,0)
-NODE_MODULE_CONTEXT_AWARE_BUILTIN(node_vec3, node_vec3::init)
-#else
 NODE_MODULE(node_vec3, node_vec3::init)
-#endif
